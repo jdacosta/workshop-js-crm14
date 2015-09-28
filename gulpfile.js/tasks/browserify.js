b893eb669ca09e797f3485caf297671fb33d231d
Julien Martins Da Costa <contact@julien-dacosta.fr>
2015-09-28 16:57:50 +0200

    Update dependencies

--- gulpfile.js/tasks/browserify.js
+++ gulpfile.js/tasks/browserify.js
@@ -1,5 +1,6 @@
 var browserify   = require('browserify');
 var gulp         = require('gulp');
+var babel        = require('gulp-babel');
 var sourcemaps   = require('gulp-sourcemaps');
 var uglify       = require('gulp-uglify');
 var gutil        = require('gulp-util');
@@ -23,6 +24,7 @@ function bundle () {
         .pipe(source(config.name))
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(babel())
         .pipe(uglify())
         .on('error', handleErrors)
         .pipe(sourcemaps.write(maps.dest))
