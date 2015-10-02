f593069b5f3288e09bdfe5eec4b1403401757ee0
Julien Martins Da Costa <contact@julien-dacosta.fr>
2015-10-02 18:23:52 +0200

    remove useless gulp task and remove useless dependencies

--- gulpfile.js/tasks/fonts.js
+++
@@ -1,9 +0,0 @@
-var gulp        = require('gulp');
-var changed     = require('gulp-changed');
-var config      = require('../config/fonts');
-
-gulp.task('fonts', function () {
-    return gulp.src(config.src)
-        .pipe(changed(config.dest))
-        .pipe(gulp.dest(config.dest));
-});
